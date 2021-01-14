7fbc886c9e7891494c7d08036581398dd8885bf5
abuzeid <Abuzeid.ibrahim@asc-me.net>
2021-01-14 23:00:49 +0100

    Initial Commit

---
+++ AB_ProblemSolving/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AB_ProblemSolving
+//
+//  Created by abuzeid on 14.01.21.
+//  Copyright © 2021 ab. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
